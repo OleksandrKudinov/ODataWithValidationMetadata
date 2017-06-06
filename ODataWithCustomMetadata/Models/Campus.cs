a551d18d9d84a1bf3a8b04cbb7674d3236097b24
Oleksandr.Kudinov <Oleksandr.Kudinov@eleks.com>
2017-06-06 13:35:10 +0300

    create two models (person and campus)

---
+++ ODataWithCustomMetadata/Models/Campus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ODataWithCustomMetadata.Models
+{
+    public class Campus
+    {
+        [Required(ErrorMessage = "Campus name is required")]
+        public String CampusName { get; set; }
+
+        [MaxLength(50, ErrorMessage = "Address is too long")]
+        public String Address { get; set; }
+
+        [MinLength(12)]
+        [MaxLength(12)]
+        public String Phone { get; set; }
+    }
+}
---
+++ ODataWithCustomMetadata/Models/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ODataWithCustomMetadata.Models
+{
+    public class Person
+    {
+        [Required]
+        [MinLength(3, ErrorMessage = "Very short")]
+        [MaxLength(30, ErrorMessage = "Very long")]
+
+        public String FirstName { get; set; }
+
+        [Required]
+        [MinLength(3, ErrorMessage = "Very short")]
+        [MaxLength(30, ErrorMessage = "Very long")]
+        public String LastName { get; set; }
+
+        [Range(18, 150, ErrorMessage = "Age is not in range 18..150")]
+        public Int32 Age { get; set; }
+    }
+}
